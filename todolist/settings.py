5efbf7736e69cdb8412c4af34e0b2e48d60b2284
Peter Francis Robante <robantepeterfran@gmail.com>
2023-09-19 12:55:05 +0800

    feat:navigation bar added

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,8 +133,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "theme/static/"
+DEBUG = True
+STATIC_URL = "/theme/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
