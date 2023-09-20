e3d4ffdb316f381f062b6724ff1ebd8c7fa0b26e
Peter Francis Robante <robantepeterfran@gmail.com>
2023-09-20 10:54:49 +0800

    feat:task delete added

--- task/urls.py
+++ task/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.task_list, name="task-list"),  # List view
     # Create (Add a Task)
     path("create/", views.create_task, name="task-create"),
+    path("delete/<int:task_id>/", views.delete_task, name="delete-task"),
+    path("task/success/", views.task_success, name="task-success"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Task
 from .forms import TaskForm
+
 
 # Create your views here.
 
@@ -17,8 +19,25 @@ def create_task(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("tast-list")
+            return redirect("task-list")
     else:
         form = TaskForm()
 
     return render(request, "task_create.html", {"form": form})
+
+
+# Delete a task
+def delete_task(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+        task.delete()
+    except Task.DoesNotExist:
+        # Handle the case where the task does not exist (optional)
+        pass
+
+    return redirect("task-list")
+
+
+# Render the task success page
+def task_success(request):
+    return render(request, "task/task_success.html")
